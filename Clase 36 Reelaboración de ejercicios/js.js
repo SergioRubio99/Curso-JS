591a26b5c2c4191ce25d6fa97e89e12e52d5e974
Sergio <srubiochavarria@gmail.com>
2021-04-24 16:23:51 +0200

    36 third

--- Clase 36 Reelaboración de ejercicios/js.js
+++ Clase 36 Reelaboración de ejercicios/js.js
@@ -11,6 +11,9 @@
 // 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+// 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
+// 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
+// 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
 
 
 
@@ -752,6 +755,7 @@ console.log(revArr)
 
 // Ejercicio 11: Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
 
+/*
 
 function factorial(n) {
 
@@ -768,3 +772,72 @@ function factorial(n) {
 }
 
 factorial(8)
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
