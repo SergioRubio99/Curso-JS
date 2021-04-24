9d7d474e140051e17ac43b4beba7b4b076640c82
Sergio <srubiochavarria@gmail.com>
2021-04-24 15:54:22 +0200

    36 first

--- Clase 36 Reelaboración de ejercicios/js.js
+++ Clase 36 Reelaboración de ejercicios/js.js
@@ -751,3 +751,15 @@ console.log(revArr)
 
 
 // Ejercicio 11: Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+
+
+function factorial(n) {
+
+
+    for (i = n; i > 0; i--) {
+        e *= n
+        console.log(e)
+    }
+}
+
+factorial(5);
