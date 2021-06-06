9312b2698bf0c27bf601dbd2a5c0c4d174e1d110
VartonX <legacyentertainmentyt@gmail.com>
2021-06-06 09:41:41 +0200

    23 y 24 acabados

--- Clase 36 Reelaboración de ejercicios/js.js
+++ Clase 36 Reelaboración de ejercicios/js.js
@@ -2053,7 +2053,8 @@ evenUneven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 
 
-//24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
+//24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }. 
+
 
 
 function lesser(arr) {
